3e625991c5998b28b2751c1bbb807a163f3cd4c4
SweetConsole <sweetconsole@gmail.com>
2026-02-03 19:11:49 +0300

    Udapte

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,6 +1,28 @@
-const swiper = new Swiper('.info_list_characters_58f7bdc3', {
+const characters = new Swiper('.info_list_characters_58f7bdc3', {
 	direction: 'horizontal',
 	loop: true,
 	spaceBetween: 15,
 	slidesPerView: 9,
+});
+
+const video_list = new Swiper('.info_list_video_58f7bdc3', {
+	direction: 'horizontal',
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.info_swiper_button_next_video_58f7bdc3',
+		prevEl: '.info_swiper_button_prev_video_58f7bdc3',
+	},
+});
+
+const photo_list = new Swiper('.info_list_photo_58f7bdc3', {
+	slidesPerView: 4,
+	grid: {
+		rows: 2,
+		fill: "row"
+	},
+	spaceBetween: 10,
+	navigation: {
+		nextEl: '.info_swiper_button_next_photo_58f7bdc3',
+		prevEl: '.info_swiper_button_prev_photo_58f7bdc3',
+	},
 });
