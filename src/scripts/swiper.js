0b087a11595df8212d1651ba574a456fe2158e40
SweetConsole <sweetconsole@gmail.com>
2026-02-04 21:14:30 +0300

    Update

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -31,3 +31,12 @@ const movies_list = new Swiper('.info_list_movies_58f7bdc3', {
 	spaceBetween: 20,
 	loop: true
 });
+
+const other_artist = new Swiper(".other_artists_58f7bdc3", {
+	slidesPerView: 6,
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.info_swiper_button_next_photo_58f7bdc3',
+		prevEl: '.info_swiper_button_prev_photo_58f7bdc3',
+	},
+})
